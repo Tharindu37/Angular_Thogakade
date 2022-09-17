1eae185c8d89cb657d465b84625a806b2fed4a16
Tharindu37 <tharindut520@gmail.com>
2022-09-17 20:59:49 +0530

    Update loadAllCustomer form

--- src/app/modules/customer/components/get-all-customer/get-all-customer.component.ts
+++ src/app/modules/customer/components/get-all-customer/get-all-customer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ToastrService} from "ngx-toastr";
+import {Customer} from "../../dto/Customer";
 
 @Component({
   selector: 'app-get-all-customer',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./get-all-customer.component.scss']
 })
 export class GetAllCustomerComponent implements OnInit {
+  loading: boolean = false;
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
 
-  constructor() { }
+  dataSet: Customer[] = [];
 
   ngOnInit(): void {
+    this.getAllCustomer();
+  }
+
+  getAllCustomer() {
+    this.loading = true;
+    this.http.get<Customer>('http://localhost:3000/api/v1/customer/getAll'
+    ).subscribe(result => {
+      // @ts-ignore
+      this.dataSet = result;
+      this.loading = false;
+    }, error=>{
+      this.loading = false;
+    })
   }
 
 }
