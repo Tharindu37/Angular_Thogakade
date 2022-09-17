2a68b6913bc4550158c934e45986eb7c2f240784
Tharindu37 <tharindut520@gmail.com>
2022-09-17 18:03:52 +0530

    Update updateCustomer form

--- src/app/modules/customer/components/update-customer/update-customer.component.ts
+++ src/app/modules/customer/components/update-customer/update-customer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ToastrService} from "ngx-toastr";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-update-customer',
@@ -6,8 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-customer.component.scss']
 })
 export class UpdateCustomerComponent implements OnInit {
+  loading = false;
 
-  constructor() { }
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
+
+  form = new FormGroup({
+    id: new FormControl(null, Validators.required),
+    name: new FormControl(null,
+      [Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
+    address: new FormControl(null,Validators.required),
+    salary: new FormControl(null,Validators.required),
+  });
+
+  submitForm(){
+    this.loading = true;
+    this.http.put("http://127.0.0.1:3000/api/v1/customer/update",{
+      id: this.form.get('id')?.value.toString(),
+      name: this.form.get('name')?.value.toString(),
+      address: this.form.get('address')?.value.toString(),
+      salary: this.form.get('salary')?.value
+    }).subscribe(result=>{
+      this.onSuccess("Updated success");
+      this.loading = false;
+    }, error=>{
+      this.onError('Try again');
+      this.loading = false;
+    })
+  }
+
+  onSuccess(title: string) {
+    this.toastr.success(title, 'Success!');
+  }
+  onError(title: string){
+    this.toastr.error(title, 'Error!');
+  }
 
   ngOnInit(): void {
   }
